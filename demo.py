8a0ed30df6b50527d0255cfe5144ba7ab2f3041f
fushisanlang <313346216@qq.com>
2020-09-24 17:36:37 +0800

    redis demo

---
+++ demo.py
@@ -0,0 +1,37 @@
+
+
+
+import json
+import redis
+
+
+class RedisTT(object):
+    def __init__(self):
+        self.host = '192.168.92.129'
+        self.port = '6379'
+        self.password = '1'
+        self.db = '1'
+        self.r = redis.StrictRedis(host=self.host, port=self.port, password=self.password, db=self.db , decode_responses=True)
+
+    def insertRedis(self, keyName, jsonStr):  # 存入到redis中
+        self.r.lpush(keyName, jsonStr)
+        print(jsonStr)
+
+
+def save1():
+    someexpert = {}
+    someexpert['id'] = 10000
+    someexpert['realname'] = 'expert-a'
+    someexpert['organization'] = 'BUAA'
+    if RedisTT().r.exists('someexpert'):
+        RedisTT().r.delete('someexpert')  # 删除key为someexpert的键值对
+    RedisTT().insertRedis(keyName='someexpert', jsonStr=json.dumps(someexpert))
+
+
+
+
+if __name__ == "__main__":
+    save1()
+
+
+    print(RedisTT().r.lrange('someexpert', 0, RedisTT().r.llen('someexpert')))
---
+++ note.md
@@ -0,0 +1,14 @@
+# note
+
+### python 操作redis
+(菜鸟笔记)[https://www.runoob.com/w3cnote/python-redis-intro.html]
+```python
+import redis
+
+r = redis.Redis(host='192.168.92.129', port=6379, db=1,password='1', decode_responses=True)
+r.set('foo', 'bar')
+print(type(r.get('foo')))
+
+
+
+```
